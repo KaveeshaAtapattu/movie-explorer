cf42e1da6b038fece5ffaf8cbf1ecf0925b72433
KaveeshaAtapattu <kaveevenuranga@gmail.com>
2025-05-08 23:26:25 +0530

    Update the Home page to show Trending movies

--- src/api.js
+++ src/api.js
@@ -3,12 +3,16 @@ import axios from 'axios';
 const API_KEY = process.env.MOVIE_APP_TMDB_API_KEY;
 const BASIC_URL = 'https://api.themoviedb.org/3';
 
+console.log('TMDB API KEY: ',API_KEY);
+
 export const fetchTrendingMovies = async () => {
     try {
         const response = await axios.get (`${BASIC_URL}/trending/movie/week`,{
-            params: { api_key:API_KEY},
+            params: { 
+                api_key:API_KEY
+            }
 
-        })
+        });
         return response.data.results;
     
     }catch (error){
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,39 @@
+import React, {useEffect, useState} from "react";
+import { fetchTrendingMovies } from '../api';
+import MovieCard from '../components/MovieCard';
+import { Grid, Typography } from "@mui/material";
+
+
 export default function Home() {
-    return <h1>Home Page</h1>;
-  }
-  
+    const[movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const loadTrending = async () => {
+          const data = await fetchTrendingMovies(); 
+          console.log('Trending Movies Response:', data); 
+          setMovies(data); 
+        };
+      
+        loadTrending(); 
+      }, []);
+      
+
+    return (
+        <div style = {{padding: '20px'}}>
+            <Typography variant="h4" gutterBottom>Trending Movies</Typography>
+            <Grid container spacing={2}>
+                {movies.length > 0 ? (
+                    movies.map(movie => (
+                        <Grid item key ={movie.id}>
+                            <MovieCard movie={movie}/>
+                        </Grid>
+                    ))
+                    ) : (
+                    <Typography>No trending movies available.</Typography>
+                )}
+                </Grid>
+            </div>
+
+        );
+    }
+
