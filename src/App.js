2e020d8438abb5784336015cb2ff727d40a59dd2
KaveeshaAtapattu <kaveevenuranga@gmail.com>
2025-05-08 19:41:05 +0530

    Typo erros corrected

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from ',/pages/Home';
-import Login from ',/pages/Login';
-import Favorites from './pages/Fovorites';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Favorites from './pages/Favorites';
 import Header from './components/Header';
 
 function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
